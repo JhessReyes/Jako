29abf249ba2c68c25c8edba93ab8d0ff1bb9b426
Sergio Santos <chejosonix@gmail.com>
2021-03-21 21:21:18 -0600

    actualizacion_1

--- Jako/Jako/Controllers/HomeController.cs
+++ Jako/Jako/Controllers/HomeController.cs
@@ -1,21 +1,16 @@
-﻿
-using System;
-using System.Data;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Jako.Models;
 
 namespace Jako.Controllers
 {
     public class HomeController : Controller
     {
-        private Nodo nodos = new Nodo();
-  //      private ArbolEntities arbol = new ArbolEntities();
         public ActionResult Index()
         {
-            return View(nodos.Listar());
+            return View();
         }
 
         public ActionResult About()
---
+++ Jako/Jako/Nodo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jako
+{
+    class Nodo {
+        public string dato;
+        public Nodo hizq;
+        public Nodo hder;
+        public Nodo padre;
+
+
+        public Nodo(string n, Nodo dad) {
+            this.dato = n;
+            this.hder = null;
+            this.hizq = null;
+            this.padre = dad;
+
+        }
+    }
+
+}
