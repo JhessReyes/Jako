ff5a70d1877ceb598ad25322c928dbfd3e658e62
Sergio Santos <chejosonix@gmail.com>
2021-04-07 17:03:57 -0600

    V7.02
    Agrege las ramas de objetos

--- Jako/Jako/Controllers/HomeController.cs
+++ Jako/Jako/Controllers/HomeController.cs
@@ -1,22 +1,20 @@
-﻿using System;
+﻿
+using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Jako.Models;
 
 namespace Jako.Controllers
 {
     public class HomeController : Controller
     {
+        private Nodo nodos = new Nodo();
+        private ArbolEntities arbol = new ArbolEntities();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
@@ -25,6 +23,56 @@ namespace Jako.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Pregunta(int id)
+        {
+            return View(nodos.ObtenerP(id));
+        }
+
+        public ActionResult Respuesta(int id)
+        {
+            ViewBag.Iden = id;
+            return View(nodos.ObtenerP(id));
+        }
+
+        public ActionResult New(int id = 0) 
+        {
+            return View(id > 0 ? nodos.ObtenerP(id)
+                : this.nodos
+                ); ;
+        }
+        public ActionResult NewQuestion(int id = 0)
+        {
+            return View(id > 0 ? nodos.ObtenerP(id)
+                : this.nodos
+                ); ;
+        }
+        public ActionResult Listo(Nodo x) 
+        {
+            ViewBag.Nod = x.ObtenerP(x.padre.GetValueOrDefault()).Id;
+
+            x.Listo();
+            return Redirect("~/Home/NewQuestion/" + (ViewBag.Nod));
+
+        }
+        public ActionResult Clo(Nodo x)
+        {
+            x.Clonar();
+            return Redirect("~/Home/New/" + x.Id);
+
+        }
+        public ActionResult Finalizar(Nodo x)
+        {
+            x.FinalizarEn();
+            ViewBag.Nod = x.ObtenerP(x.padre.GetValueOrDefault()).hizq;
+            return Redirect("/Home");
+
+        }
+        public ActionResult Editar(int id = 1)
+        {
+            return View();
+
         }
     }
 }
--- Jako/Jako/Models/Nodo.cs
+++ Jako/Jako/Models/Nodo.cs
@@ -4,9 +4,8 @@ namespace Jako.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Linq;
-    using System.Data.Entity.Spatial;
-    using Jako.Models;
 
     [Table("Nodo")]
     public partial class Nodo
@@ -36,6 +35,103 @@ namespace Jako.Models
                 throw;
             }
             return nodo;
+        }
+        public Nodo ObtenerP(int id)
+        {
+            var preg = new Nodo();
+            try
+            {
+                using (var context = new Model1())
+                {
+                    preg = context.Nodo
+                                  .Where(x => x.Id == id)
+                                  .Single();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return preg;
+        }
+
+        public void Clonar() {
+            try
+            {               
+                using (var context = new Model1())
+                {
+  
+                    Nodo cl = (Nodo)this.MemberwiseClone();
+                    if (cl.Id > 0)
+                    {
+                        context.Entry(cl).State = EntityState.Added;
+
+                    }
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Listo()
+        {
+            try
+            {
+                using (var context = new Model1())
+                {
+                    if (this.Id > 0) 
+                    {
+                        context.Entry(this).State = EntityState.Added;
+                        
+                    }
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public void FinalizarEn()
+        {
+            try
+            {
+                using (var context = new Model1())
+                {
+                    if (this.Id > 0)
+                    {
+                        context.Entry(this).State = EntityState.Modified;
+
+                    }
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public int LastId()
+        {
+            var id = 1;
+            var nodo = new List<Nodo>();
+            try
+            {
+                using (var context = new Model1())
+                {
+                    nodo = context.Nodo.ToList();
+                    id = nodo.Last().Id;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return id;
         }
 
     }
